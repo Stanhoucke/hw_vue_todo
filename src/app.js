0d46277aaafd38da4517057048ecc243a0513fc1
Stanley Houcke <stanhoucke@gmail.com>
2021-01-11 15:50:12 +0000

    todo list shows in browser

--- src/app.js
+++ src/app.js
@@ -4,7 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
   new Vue({
     el: "#app",
     data: {
-      
+      todos: [
+          {name: "Buy shopping", priority: "High"},
+          {name: "Clean bathroom", priority: "Low"},
+          {name: "Car's MOT", priority: "High"}
+      ]
     },
     methods: {
       
